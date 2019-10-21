1ff1178f5595d3fcd9d56713a1f0bc0cfa59abd1
shinkit314 <zhenyujie19920314@yahoo.co.jp>
2019-10-21 15:37:28 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -46,7 +46,7 @@ def main(yolo):
         w = int(video_capture.get(3))
         h = int(video_capture.get(4))
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        out = cv2.VideoWriter(sys.argv[1] + '_output.avi', fourcc, 15, (w, h))
+        out = cv2.VideoWriter(sys.argv[1] + '_output.avi', fourcc, 30, (w, h))
         list_file = open(sys.argv[1] + '_detection.txt', 'w')
         frame_index = -1 
         
